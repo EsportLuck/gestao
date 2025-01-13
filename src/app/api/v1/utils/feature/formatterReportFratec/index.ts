cc53fd395d8f59c84ce93b2076eb45ad2fd5b722
OliverioJunior <oliverio.junior2@gmail.com>
2025-01-13 18:26:41 -0300

    feat: criar tipagem de estrutura de relatorio de loteria

--- src/app/api/v1/utils/feature/formatterReportFratec/index.ts
+++ src/app/api/v1/utils/feature/formatterReportFratec/index.ts
@@ -5,7 +5,15 @@ import { filterEstablishmentsWithoutSales } from "./utils/filterEstablishmentsWi
 import { TReportFratec } from "@/app/api/v1/types";
 import { WorkSheet } from "xlsx";
 import { xlsxReader } from "@/app/api/controller";
-
+export type TFileEstrutura = {
+  Localidade: string;
+  "Código Ponto": string;
+  Nome: string;
+  Vendas: string;
+  Comissão: string;
+  "Prêmios Pagos": string;
+  Líquido: string;
+};
 export const formatterReportFratec = (worksheet: WorkSheet) => {
   const json = xlsxReader.toJson(worksheet);
   const reportWithKeys = changeKeysObjectFratec(json);
