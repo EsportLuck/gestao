8b1969f433a6b92440cd07a7d97f0424145a6563
OliverioJunior <oliverio.junior2@gmail.com>
2025-01-14 17:56:20 -0300

    refacto: refatorar função futebol arena e adicionar comissão retida

--- src/app/api/utils/gravaDadosFutebol.ts
+++ src/app/api/utils/gravaDadosFutebol.ts
@@ -1,9 +1,10 @@
 import { prisma } from "@/services/prisma";
-import { registerReportFutebol } from "@/app/api/v1/utils/create";
-import { Localidade, Secao } from "@prisma/client";
+import { Localidade, Prisma, Secao } from "@prisma/client";
 import { EstabelecimentoSelecionado } from "@/app/api/contracts";
 import { TReportAtena } from "@/app/api/v1/types";
 import { FormatterFunctions } from "@/app/api/v1/utils/strategy";
+import { obterInicioEFimDoCiclo } from "../v1/utils/obterInicioEFimDoCiclo";
+import { formatarData } from "@/utils/formatarData";
 
 export const gravaDadosFutebol = async (
   file: TReportAtena[],
@@ -14,118 +15,238 @@ export const gravaDadosFutebol = async (
   localidadesNoBanco: Localidade[],
   secaoNoBanco: Secao[],
   importacaoId: number,
+  tx: Prisma.TransactionClient,
 ) => {
-  const localidadesDoArquivo = file.map((dadosDosEstabelecimentos) => {
-    return {
-      name: dadosDosEstabelecimentos.Localidade,
-      empresa: {
-        connect: {
-          name: company,
-        },
-      },
-    };
-  });
-  const secaoDoArquivo = file.map((dadosDosEstabelecimentos) => {
-    return {
-      name: dadosDosEstabelecimentos.Seção,
-      localidade: {
-        connect: {
-          name: dadosDosEstabelecimentos.Localidade,
-        },
-      },
-      empresa: {
-        connect: {
-          name: company,
-        },
-      },
-    };
-  });
+  try {
+    const { localidadesSuccess } = await criarLocalidadesInexistentes(
+      localidadesNoBanco,
+      file.map((item) => {
+        return {
+          name: item.Localidade,
+          empresa: { connect: { name: company } },
+        };
+      }),
+      tx,
+    );
+    const { secaoSuccess } = await criarSecaoInexistentes(
+      secaoNoBanco,
+      file.map((item) => {
+        return {
+          name: item.Seção,
+          localidade: { connect: { name: item.Localidade } },
+          empresa: { connect: { name: company } },
+        };
+      }),
+      localidadesSuccess,
+      tx,
+    );
 
-  const estabelecimentosDoArquivo = file.map((dadosDosEstabelecimentos) => {
-    return {
-      name: dadosDosEstabelecimentos.Estabelecimento,
-      site,
-      empresa: {
-        connect: {
-          name: company,
-        },
-      },
-      localidade: {
-        connect: {
-          name: dadosDosEstabelecimentos.Localidade,
-        },
-      },
-      secao: {
-        connect: {
-          name: dadosDosEstabelecimentos.Seção,
-        },
-      },
-    };
-  });
-
-  const dadosNoSistema = await prisma.$transaction(
-    async () => {
-      const { localidadesSuccess } = await criarLocalidadesInexistentes(
-        localidadesNoBanco,
-        localidadesDoArquivo,
-      );
-      const { secaoSuccess } = await criarSecaoInexistentes(
-        secaoNoBanco || [],
-        secaoDoArquivo,
-        localidadesSuccess,
-      );
-
-      const { estabelecimentosSuccess } =
-        await criarEstabelecimentosInexistentes(
-          estabelecimentosNoBanco,
-          estabelecimentosDoArquivo,
-          localidadesSuccess,
-          secaoSuccess,
-        );
-
-      if (estabelecimentosSuccess) {
-        const estabelecimentos = await prisma.estabelecimento.findMany({
-          where: {
-            empresa: {
-              name: company,
+    const { estabelecimentosSuccess } = await criarEstabelecimentosInexistentes(
+      estabelecimentosNoBanco,
+      file.map((item) => {
+        return {
+          name: item.Estabelecimento,
+          site,
+          empresa: {
+            connect: { name: company },
+          },
+          companies: {
+            connect: {
+              id: 1,
             },
           },
-          select: {
-            id: true,
-            name: true,
-            matrizId: true,
+          localidade: {
+            connect: {
+              name: item.Localidade,
+            },
+          },
+          secao: {
+            connect: {
+              name: item.Seção,
+            },
+          },
+        };
+      }),
+      localidadesSuccess,
+      secaoSuccess,
+      tx,
+    );
+
+    if (!localidadesSuccess || !secaoSuccess || !estabelecimentosSuccess)
+      return { success: false, message: "Algo deu errado na criação de dados" };
+
+    const todosOsEstabelecimentosNoBanco = await tx.estabelecimento.findMany({
+      where: {
+        name: {
+          in: file.map((item) => item.Estabelecimento),
+        },
+      },
+    });
+
+    for await (const relatorio of file) {
+      let estabelecimento = todosOsEstabelecimentosNoBanco.find(
+        (item) => item.name === relatorio.Estabelecimento,
+      );
+      if (!estabelecimento?.id) {
+        estabelecimento = await tx.estabelecimento.create({
+          data: {
+            name: relatorio.Estabelecimento,
+            site,
+            empresa: {
+              connect: {
+                name: company,
+              },
+            },
+            companies: {
+              connect: {
+                id: 1,
+              },
+            },
+            localidade: {
+              connect: {
+                name: relatorio.Localidade,
+              },
+            },
+            secao: {
+              connect: {
+                name: relatorio.Seção,
+              },
+            },
           },
         });
-
-        for (const dadosEstabelecimento of file) {
-          const estabelecimento = estabelecimentos.find(
-            (estab) => estab.name === dadosEstabelecimento.Estabelecimento,
-          );
-
-          await registerReportFutebol(
-            estabelecimento?.id as number,
-            weekReference,
-            site,
-            dadosEstabelecimento.Vendas,
-            dadosEstabelecimento.Quantidade,
-            dadosEstabelecimento.Comissão,
-            dadosEstabelecimento["Prêmios/Saques"],
-            dadosEstabelecimento.Líquido,
-            importacaoId,
-            estabelecimento?.matrizId as number,
-          );
-        }
-
-        return { success: true, message: "Importado com sucesso" };
       }
-    },
-    { timeout: 10000 },
-  );
+      const validarComissaoRetida = estabelecimento.comissao_retida;
 
-  if (dadosNoSistema?.success)
-    return { success: dadosNoSistema.success, message: dadosNoSistema.message };
+      await tx.vendas.create({
+        data: {
+          establishmentId: estabelecimento.id,
+          referenceDate: new Date(weekReference),
+          value: relatorio.Vendas,
+          site,
+          quantity: relatorio.Quantidade,
+          importacaoId,
+        },
+      });
+      await tx.comissao.create({
+        data: {
+          establishmentId: estabelecimento.id,
+          referenceDate: new Date(weekReference),
+          value: relatorio.Comissão,
+          site,
+          importacaoId,
+        },
+      });
+      await tx.premios.create({
+        data: {
+          establishmentId: estabelecimento.id,
+          referenceDate: new Date(weekReference),
+          value: relatorio["Prêmios/Saques"],
+          site,
+          importacaoId,
+        },
+      });
+      await tx.liquido.create({
+        data: {
+          establishmentId: estabelecimento.id,
+          referenceDate: new Date(weekReference),
+          value: relatorio.Líquido,
+          site,
+          importacaoId,
+        },
+      });
+      const ultimoCaixa = await tx.caixa.findFirst({
+        where: {
+          establishmentId: estabelecimento.id,
+        },
+        orderBy: {
+          referenceDate: "desc",
+        },
+      });
+      const { inicioDoCiclo: gte, finalDoCiclo: lte } =
+        obterInicioEFimDoCiclo(weekReference);
 
-  return { success: false, message: "Não foi possível importar os dados" };
+      const ciclo = await tx.ciclo.findFirst({
+        where: {
+          reference_date: {
+            gte,
+            lte,
+          },
+          establishmentId: estabelecimento.id,
+        },
+      });
+
+      if (!ciclo?.id) {
+        await tx.ciclo.create({
+          data: {
+            reference_date: formatarData(weekReference.toISOString()),
+            establishmentId: estabelecimento.id,
+            status: "PENDENTE",
+          },
+        });
+      }
+
+      if (
+        ultimoCaixa?.referenceDate.getUTCDate() !==
+        formatarData(weekReference.toISOString()).getUTCDate()
+      ) {
+        await tx.caixa.create({
+          data: {
+            establishmentId: estabelecimento.id,
+            referenceDate: new Date(weekReference),
+            total: validarComissaoRetida
+              ? relatorio.Líquido +
+                (ultimoCaixa?.total || 0) +
+                relatorio.Comissão
+              : relatorio.Líquido + (ultimoCaixa?.total || 0),
+            importacaoId,
+            status: "PENDENTE",
+            value_futebol: relatorio.Líquido,
+          },
+        });
+      } else {
+        await tx.caixa.update({
+          where: {
+            id: ultimoCaixa.id,
+          },
+          data: {
+            value_futebol: {
+              increment: relatorio.Líquido,
+            },
+            total: {
+              increment: validarComissaoRetida
+                ? relatorio.Líquido + relatorio.Comissão
+                : relatorio.Líquido,
+            },
+          },
+        });
+      }
+    }
+
+    return { success: true, message: "Dados importados com sucesso" };
+  } catch (error) {
+    if (
+      error instanceof Error ||
+      error instanceof SyntaxError ||
+      error instanceof TypeError ||
+      error instanceof Prisma.PrismaClientKnownRequestError ||
+      error instanceof Prisma.PrismaClientUnknownRequestError ||
+      error instanceof Prisma.PrismaClientRustPanicError ||
+      error instanceof Prisma.PrismaClientValidationError ||
+      error instanceof Prisma.PrismaClientUnknownRequestError ||
+      error instanceof Prisma.PrismaClientRustPanicError ||
+      error instanceof Prisma.PrismaClientValidationError ||
+      error instanceof Prisma.PrismaClientUnknownRequestError
+    ) {
+      console.error(" Erro no servidor importação Futebol ", error.message);
+      return {
+        sucess: false,
+        message: error.message,
+      };
+    } else {
+      console.error(" Erro no servidor importação Futebol ", error);
+      return { success: false, message: "Erro desconhecido" };
+    }
+  }
 };
 
 const criarLocalidadesInexistentes = async (
@@ -138,6 +259,7 @@ const criarLocalidadesInexistentes = async (
       };
     };
   }[],
+  tx: Prisma.TransactionClient,
 ) => {
   try {
     const localidadesSemRepetir = localidadesNoArquivo.filter(
@@ -151,7 +273,7 @@ const criarLocalidadesInexistentes = async (
     );
 
     for (const item of localidadesParaCriar) {
-      await prisma.localidade.create({
+      await tx.localidade.create({
         data: {
           name: item.name,
           empresa: {
@@ -185,6 +307,7 @@ const criarSecaoInexistentes = async (
     };
   }[],
   localidadesSuccess: boolean,
+  tx: Prisma.TransactionClient,
 ) => {
   try {
     if (!localidadesSuccess) return { secaoSuccess: false };
@@ -198,7 +321,7 @@ const criarSecaoInexistentes = async (
     });
 
     for (const item of secaoParaCriar) {
-      await prisma.secao.create({
+      await tx.secao.create({
         data: {
           name: item.name,
           Localidade: {
@@ -245,6 +368,7 @@ const criarEstabelecimentosInexistentes = async (
   }[],
   localidadesSuccess: boolean,
   secaoSuccess: boolean,
+  tx: Prisma.TransactionClient,
 ) => {
   try {
     if (!localidadesSuccess || !secaoSuccess)
@@ -258,7 +382,7 @@ const criarEstabelecimentosInexistentes = async (
     );
 
     for (const item of estabelecimentosParaCriar) {
-      await prisma.estabelecimento.create({
+      await tx.estabelecimento.create({
         data: {
           name: item.name.trim(),
           site: item.site,
