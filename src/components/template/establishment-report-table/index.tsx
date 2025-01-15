3b3a1c44cdb2a3ebb9fcee23f8ba03c4a9a80481
OliverioJunior <oliverio.junior2@gmail.com>
2025-01-15 17:57:02 -0300

    fix: consertar bug na pagina de estabelecimentos

--- src/components/template/establishment-report-table/index.tsx
+++ src/components/template/establishment-report-table/index.tsx
@@ -1,34 +1,26 @@
+"use client";
 import { TEstabelecimento } from "@/types/estabelecimento";
 import { TReportEstablishment, columns } from "./columns";
 import { DataTable } from "./data-table";
+import { useMemo } from "react";
+import { useFetch } from "@/hooks/useFetch";
 
-async function getData(): Promise<TReportEstablishment[]> {
-  try {
-    const response = await fetch(
-      `${process.env.API_URL}/management/establishments`,
-      {
-        cache: "no-store",
-      },
-    );
-    if (!response.ok) throw new Error("Failed to fetch data");
-    const estabelecimento: TEstabelecimento[] = await response.json();
-    const estabelecimentos: TReportEstablishment[] = estabelecimento.map(
-      (item) => {
-        return {
-          id: item.id.toString(),
-          estabelecimento: item.name,
-        };
-      },
-    );
-    if (estabelecimentos.length === 0) return [];
-    return estabelecimentos;
-  } catch (err) {
-    return [];
-  }
-}
+export default function EstablishmentReportTable() {
+  const dataEstabelecimento = useFetch<TEstabelecimento[]>(
+    "/api/v1/management/establishments",
+  );
+  const data = useMemo<TReportEstablishment[]>((): TReportEstablishment[] => {
+    const estabelecimentos = dataEstabelecimento.data;
+    if (!estabelecimentos) return [];
+    return estabelecimentos.map((item) => {
+      const data: TReportEstablishment = {
+        id: item.id.toString(),
+        estabelecimento: item.name,
+      };
+      return data;
+    });
+  }, [dataEstabelecimento.data]);
 
-export async function EstablishmentReportTable() {
-  const data = await getData();
   return (
     <div className="container mx-auto mt-10 p-0">
       <DataTable columns={columns} data={data} />
