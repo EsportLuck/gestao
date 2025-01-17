f62df4b76779d0438a8247ff77a26d0fe052ffc0
OliverioJunior <oliverio.junior2@gmail.com>
2025-01-17 17:32:18 -0300

    refacto: atualizar rota de criar caixa personalizado para empresa

--- src/app/api/v1/(routes)/cronjob/route.ts
+++ src/app/api/v1/(routes)/cronjob/route.ts
@@ -1,20 +1,12 @@
-import {
-  CaixaRepository,
-  CronJobCriarCaixaDiarioRepository,
-  EstabelecimentoRepository,
-} from "@/app/api/repositories";
-import {
-  CaixaService,
-  CronJobCriarCaixaDiarioService,
-  EstabelecimentoService,
-} from "@/app/api/services";
 import { prisma } from "@/services/prisma";
 import { NextRequest, NextResponse } from "next/server";
-
+import { obterDiaAnterior } from "@/app/api/v1/utils/obterDiaAnterior";
+import { Prisma } from "@prisma/client";
 export const revalidate = 0;
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
+
   if (
     req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
   ) {
@@ -23,36 +15,89 @@ export async function GET(req: NextRequest) {
       { status: 401 },
     );
   }
-  const estabelecimentoService = new EstabelecimentoService(
-    new EstabelecimentoRepository(),
-  );
-  const caixaService = new CaixaService(new CaixaRepository());
-  const cronJobService = new CronJobCriarCaixaDiarioService(
-    new CronJobCriarCaixaDiarioRepository(),
-    estabelecimentoService,
-    caixaService,
-  );
+
   try {
+    const data = searchParams.get("date");
+    const empresa = searchParams.get("empresa");
+    const user = searchParams.get("user");
+    if (!data || !empresa || !user) {
+      return { success: false, message: "Dados inválidos" };
+    }
     const { success, message } = await prisma.$transaction(
-      async () => {
-        const params = searchParams.get("date");
-        const date = new Date(params || new Date());
-        const { success, message } =
-          await cronJobService.executarCronjobCaixa(date);
-        if (!success) {
-          return { success, message };
+      async (tx) => {
+        const { startOfDay } = obterDiaAnterior(data);
+        const todosEstabelecimentos = await tx.estabelecimento.findMany({
+          where: {
+            empresa: {
+              name: empresa,
+            },
+          },
+          select: {
+            id: true,
+            name: true,
+            caixa: {
+              where: {
+                referenceDate: startOfDay,
+              },
+            },
+          },
+        });
+        for await (const item of todosEstabelecimentos) {
+          const caixa = item.caixa?.[0];
+
+          if (typeof caixa.id !== "number") {
+            await tx.caixa.create({
+              data: {
+                referenceDate: startOfDay,
+                status: "PENDENTE",
+                total: 0,
+                importacaoId: null,
+                establishmentId: item.id,
+                createdBy: `CRONJOB ${data}  ${user}`,
+              },
+            });
+          } else {
+            await tx.caixa.create({
+              data: {
+                referenceDate: new Date(data),
+                status: "PENDENTE",
+                total: caixa.total,
+                importacaoId: null,
+                establishmentId: item.id,
+                createdBy: `CRONJOB ${data}  ${user}`,
+              },
+            });
+          }
         }
-        return { success, message };
+        return { success: true, message: "Caixas criadas com sucesso" };
       },
       { timeout: 20000 },
     );
+
     if (!success) {
       return NextResponse.json({ success: false, message }, { status: 500 });
     }
 
     return NextResponse.json({ success, message }, { status: 200 });
   } catch (error) {
-    const message = error instanceof Error ? error.message : error;
-    return NextResponse.json({ success: false, message }, { status: 500 });
+    if (
+      error instanceof Error ||
+      error instanceof SyntaxError ||
+      error instanceof TypeError ||
+      error instanceof Prisma.PrismaClientKnownRequestError ||
+      error instanceof Prisma.PrismaClientValidationError ||
+      error instanceof Prisma.PrismaClientUnknownRequestError ||
+      error instanceof Prisma.PrismaClientRustPanicError
+    ) {
+      return NextResponse.json(
+        { success: false, message: error.message },
+        { status: 500 },
+      );
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Erro desconhecido",
+      });
+    }
   }
 }
