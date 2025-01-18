dd3b4747d84e53c5c7f75c085246785394480637
OliverioJunior <oliverio.junior2@gmail.com>
2025-01-18 11:20:07 -0300

    remover restrições

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,34 +9,34 @@ const admRoutes = ["/dashboard/register"];
 const supervisorRoutes = ["/dashboard", `/dashboard/extrato`, "/"];
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
-  const sendToDashboard = () =>
-    NextResponse.redirect(new URL(DASHBOARD, request.url));
-  const sendToHome = () => NextResponse.redirect(new URL(HOME, request.url));
-  if (
-    token?.role === "supervisor" &&
-    !supervisorRoutes.includes(request.nextUrl.pathname)
-  )
-    return sendToDashboard();
-  if (!token && request.nextUrl.pathname !== HOME) return sendToHome();
-  if (token && request.nextUrl.pathname === HOME) return sendToDashboard();
-  if (
-    token?.role !== "administrador" &&
-    admRoutes.includes(request.nextUrl.pathname)
-  ) {
-    return sendToDashboard();
-  }
+  // const token = await getToken({ req: request });
+  // const sendToDashboard = () =>
+  //   NextResponse.redirect(new URL(DASHBOARD, request.url));
+  // const sendToHome = () => NextResponse.redirect(new URL(HOME, request.url));
+  // if (
+  //   token?.role === "supervisor" &&
+  //   !supervisorRoutes.includes(request.nextUrl.pathname)
+  // )
+  //   return sendToDashboard();
+  // if (!token && request.nextUrl.pathname !== HOME) return sendToHome();
+  // if (token && request.nextUrl.pathname === HOME) return sendToDashboard();
+  // if (
+  //   token?.role !== "administrador" &&
+  //   admRoutes.includes(request.nextUrl.pathname)
+  // ) {
+  //   return sendToDashboard();
+  // }
 }
 
 export const config = {
-  matcher: [
-    "/",
-    "/dashboard",
-    "/dashboard/:path*",
-    "/api/v1/:path*",
-    {
-      source:
-        "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
-    },
-  ],
+  // matcher: [
+  //   "/",
+  //   "/dashboard",
+  //   "/dashboard/:path*",
+  //   "/api/v1/:path*",
+  //   {
+  //     source:
+  //       "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  //   },
+  // ],
 };
