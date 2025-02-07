aeb85ef9293402b4a75666d9884abdc72b055151
OliverioJunior <oliverio.junior2@gmail.com>
2025-02-07 10:58:48 -0300

    fix: consertar erro que impedia que o relatório do `Arena.site` fosse atualizado

--- src/app/api/v1/(routes)/import/atualizar/route.ts
+++ src/app/api/v1/(routes)/import/atualizar/route.ts
@@ -12,13 +12,21 @@ import {
 } from "@/app/api/services";
 import { prisma } from "@/services/prisma";
 import { NextRequest } from "next/server";
-import { FormatterFunctions } from "@/app/api/v1/utils/strategy";
-import { TReportAtena } from "../../../types";
+import {
+  FormatterFunctions,
+  ReportCategory,
+  reportSites,
+} from "@/app/api/v1/utils/strategy";
+import {
+  TReportAtena,
+  TFormattedReportArenaSite,
+  IFormattedReportSportNet,
+} from "@/app/api/v1/types";
 
 export async function PATCH(request: NextRequest) {
   const response = await request.formData();
   const formDataWeekReference = response.get("weekReference");
-  const formDataSite = response.get("site");
+  const formDataSite = response.get("site") as string;
   const formDataIdImportacao = response.get("idImportacao");
   const formDataUser = response.get("user");
   const formDataFile = response.get("file");
@@ -40,7 +48,6 @@ export async function PATCH(request: NextRequest) {
       formDataFile as File,
       formDataSite as keyof FormatterFunctions,
     );
-
     if (!success) {
       return new Response(message, { status: 500 });
     }
@@ -98,66 +105,142 @@ export async function PATCH(request: NextRequest) {
                 value_futebol: true,
               },
             });
-            const estabelecimentos = file as TReportAtena[];
-            for await (const estabelecimento of estabelecimentos) {
-              const premioDoBanco = importacao?.Premios.find(
-                (premioDoEstabelecimento) =>
-                  premioDoEstabelecimento.establishment.name ===
-                  estabelecimento.Estabelecimento,
-              );
-              const premioDoArquivo = Number(
-                (estabelecimento["Prêmios/Saques"] * 100).toFixed(),
-              );
+            const arenaSite =
+              reportSites[ReportCategory.ARENA_SITE].includes(formDataSite);
+            const arena =
+              reportSites[ReportCategory.ARENA].includes(formDataSite);
+            const esportebet =
+              reportSites[ReportCategory.BET].includes(formDataSite);
+            const esportevip =
+              reportSites[ReportCategory.VIP].includes(formDataSite);
+            if (arenaSite) {
+              const estabelecimentos = file as TFormattedReportArenaSite[];
+              for await (const estabelecimento of estabelecimentos) {
+                const premioDoBanco = importacao?.Premios.find(
+                  (premioDoEstabelecimento) =>
+                    premioDoEstabelecimento.establishment.name ===
+                    estabelecimento.estabelecimento,
+                );
+                const premioDoArquivo = Number(
+                  (estabelecimento.premios * 100).toFixed(),
+                );
 
-              if (premioDoBanco?.value === premioDoArquivo) continue;
-              const caixasDoEstabelecimentos = caixas.filter(
-                (caixa) =>
-                  caixa.establishment.name === estabelecimento.Estabelecimento,
-              );
-              const liquidoDoBanco = importacao?.Liquido.find(
-                (liquindoDoEstabelecimento) =>
-                  liquindoDoEstabelecimento.establishment.name ===
-                  estabelecimento.Estabelecimento,
-              );
+                if (premioDoBanco?.value === premioDoArquivo) continue;
+                const caixasDoEstabelecimentos = caixas.filter(
+                  (caixa) =>
+                    caixa.establishment.name ===
+                    estabelecimento.estabelecimento,
+                );
+                const liquidoDoBanco = importacao?.Liquido.find(
+                  (liquindoDoEstabelecimento) =>
+                    liquindoDoEstabelecimento.establishment.name ===
+                    estabelecimento.estabelecimento,
+                );
 
-              const diferençaNoLiquido =
-                (liquidoDoBanco?.value || 0) -
-                Number((estabelecimento.Líquido * 100).toFixed());
+                const diferençaNoLiquido =
+                  (liquidoDoBanco?.value || 0) -
+                  Number((estabelecimento.liquido * 100).toFixed());
+                console.log({ liquidoDoBanco });
+                await liquidoService.update(liquidoDoBanco?.id as number, {
+                  value: Number((estabelecimento.liquido * 100).toFixed()),
+                  updatedAt: new Date(),
+                });
 
-              await liquidoService.update(liquidoDoBanco?.id as number, {
-                value: Number((estabelecimento.Líquido * 100).toFixed()),
-                updatedAt: new Date(),
-              });
+                await premiosService.update(premioDoBanco?.id as number, {
+                  value: premioDoArquivo,
+                  updatedAt: new Date(),
+                });
 
-              await premiosService.update(premioDoBanco?.id as number, {
-                value: premioDoArquivo,
-                updatedAt: new Date(),
-              });
-
-              await tx.caixa.updateMany({
-                where: {
-                  id: {
-                    in: caixasDoEstabelecimentos.map((item) => item.id),
+                await tx.caixa.updateMany({
+                  where: {
+                    id: {
+                      in: caixasDoEstabelecimentos.map((item) => item.id),
+                    },
                   },
+                  data: {
+                    total: {
+                      decrement: diferençaNoLiquido,
+                    },
+                    value_futebol: {
+                      decrement: diferençaNoLiquido,
+                    },
+                  },
+                });
+              }
+              await importacaoService.atualizarImportacao(
+                Number(formDataIdImportacao),
+                {
+                  state: "Atualizado",
+                  modifiedBy: formDataUser.toString(),
+                  updatedAt: new Date(),
                 },
-                data: {
-                  total: {
-                    decrement: diferençaNoLiquido,
-                  },
-                  value_futebol: {
-                    decrement: diferençaNoLiquido,
-                  },
-                },
-              });
+              );
             }
-            await importacaoService.atualizarImportacao(
-              Number(formDataIdImportacao),
-              {
-                state: "Atualizado",
-                modifiedBy: formDataUser.toString(),
-                updatedAt: new Date(),
-              },
-            );
+            if (esportevip || esportebet || arena) {
+              const estabelecimentos = file as IFormattedReportSportNet[];
+
+              for await (const estabelecimento of estabelecimentos) {
+                const premioDoBanco = importacao?.Premios.find(
+                  (premioDoEstabelecimento) =>
+                    premioDoEstabelecimento.establishment.name ===
+                    estabelecimento.Estabelecimento,
+                );
+                const premioDoArquivo = Number(
+                  (estabelecimento["Prêmios/Saques"] * 100).toFixed(),
+                );
+
+                if (premioDoBanco?.value === premioDoArquivo) continue;
+                const caixasDoEstabelecimentos = caixas.filter(
+                  (caixa) =>
+                    caixa.establishment.name ===
+                    estabelecimento.Estabelecimento,
+                );
+                const liquidoDoBanco = importacao?.Liquido.find(
+                  (liquindoDoEstabelecimento) =>
+                    liquindoDoEstabelecimento.establishment.name ===
+                    estabelecimento.Estabelecimento,
+                );
+
+                const diferençaNoLiquido =
+                  (liquidoDoBanco?.value || 0) -
+                  Number((estabelecimento.Líquido * 100).toFixed());
+                console.log({ liquidoDoBanco });
+                await liquidoService.update(liquidoDoBanco?.id as number, {
+                  value: Number((estabelecimento.Líquido * 100).toFixed()),
+                  updatedAt: new Date(),
+                });
+
+                await premiosService.update(premioDoBanco?.id as number, {
+                  value: premioDoArquivo,
+                  updatedAt: new Date(),
+                });
+
+                await tx.caixa.updateMany({
+                  where: {
+                    id: {
+                      in: caixasDoEstabelecimentos.map((item) => item.id),
+                    },
+                  },
+                  data: {
+                    total: {
+                      decrement: diferençaNoLiquido,
+                    },
+                    value_futebol: {
+                      decrement: diferençaNoLiquido,
+                    },
+                  },
+                });
+              }
+              await importacaoService.atualizarImportacao(
+                Number(formDataIdImportacao),
+                {
+                  state: "Atualizado",
+                  modifiedBy: formDataUser.toString(),
+                  updatedAt: new Date(),
+                },
+              );
+            }
+
             return { success: true, message: "Atualizado com sucesso" };
           } catch (err) {
             console.error(err);
