1ee1948701f495cebac7a2ed78cb8451f0417ac8
OliverioJunior <oliverio.junior2@gmail.com>
2025-02-27 17:25:00 -0300

    feat: criar hook para Rota

---
+++ src/hooks/useRota.ts
@@ -0,0 +1,24 @@
+import { useMemo } from "react";
+import { Rota } from "@prisma/client";
+import { useFetch } from "@/hooks/useFetch";
+
+export function useRota() {
+  const obterRota = useFetch<{ rotas: Partial<Rota>[] }>(
+    "/api/v1/management/routes",
+  );
+  const rotas = useMemo(() => {
+    if (obterRota.data && obterRota.data.rotas?.length > 0) {
+      return obterRota.data.rotas.map((rota) => ({
+        ...rota,
+        name: rota.name,
+      }));
+    }
+    return [];
+  }, [obterRota.data]);
+
+  return {
+    rotas,
+    isLoading: obterRota.isLoading,
+    error: obterRota.error,
+  };
+}
