c11d6049bbe8eb394bc16f11aaa955159770c345
OliverioJunior <oliverio.junior2@gmail.com>
2025-02-27 17:24:09 -0300

    feat: criar hook para secoes

---
+++ src/hooks/useSecoes.ts
@@ -0,0 +1,24 @@
+import { useMemo } from "react";
+import { Secao } from "@prisma/client";
+import { useFetch } from "@/hooks/useFetch";
+
+export function useSecoes() {
+  const obterSecoes = useFetch<{ secoes: Partial<Secao>[] }>(
+    "/api/v1/management/sections",
+  );
+  const secao = useMemo(() => {
+    if (obterSecoes.data && obterSecoes.data.secoes?.length > 0) {
+      return obterSecoes.data.secoes.map((secao) => ({
+        ...secao,
+        name: secao.name,
+      }));
+    }
+    return [];
+  }, [obterSecoes.data]);
+
+  return {
+    secao,
+    isLoading: obterSecoes.isLoading,
+    error: obterSecoes.error,
+  };
+}
