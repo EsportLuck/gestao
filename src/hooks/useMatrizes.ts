c95b8adf9fec30c9883903934e837318443d1ecb
OliverioJunior <oliverio.junior2@gmail.com>
2025-03-01 17:36:18 -0300

    feat: criar hook para obter Matrizes

---
+++ src/hooks/useMatrizes.ts
@@ -0,0 +1,28 @@
+import { useMemo } from "react";
+import { Estabelecimento } from "@prisma/client";
+import { useFetch } from "@/hooks/useFetch";
+
+type TMatrizResponse = {
+  matrizes: Partial<Estabelecimento>[];
+};
+
+export function useMatrizes() {
+  const { data, error, isLoading } = useFetch<TMatrizResponse>(
+    "/api/v1/management/companies",
+  );
+  const matrizes = useMemo(() => {
+    if (!data || isLoading === true) {
+      return null;
+    }
+    return data.matrizes.map((matriz) => ({
+      ...matriz,
+      name: matriz.name,
+    }));
+  }, [data, isLoading]);
+
+  return {
+    matrizes,
+    isLoading,
+    error,
+  };
+}
