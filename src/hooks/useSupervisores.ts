8db6a136141a69ab484bd604ef599cdd01b821ba
OliverioJunior <oliverio.junior2@gmail.com>
2025-03-04 07:11:46 -0300

    refactor: dar mais legibilidade ao codigo

--- src/hooks/useSupervisores.ts
+++ src/hooks/useSupervisores.ts
@@ -3,25 +3,19 @@ import { Supervisor } from "@prisma/client";
 import { useFetch } from "@/hooks/useFetch";
 
 export function useSupervisores() {
-  const obterSupervisores = useFetch<{ supervisores: Partial<Supervisor>[] }>(
-    "/api/v1/management/supervisores",
-  );
+  const { data, error, isLoading } = useFetch<{
+    supervisores: Partial<Supervisor>[];
+  }>("/api/v1/management/supervisores");
   const supervisores = useMemo(() => {
-    if (
-      obterSupervisores.data &&
-      obterSupervisores.data.supervisores?.length > 0
-    ) {
-      return obterSupervisores.data.supervisores.map((supervisor) => ({
-        ...supervisor,
-        name: supervisor.name,
-      }));
+    if (!data || isLoading) {
+      return null;
     }
-    return [];
-  }, [obterSupervisores.data]);
+    return data.supervisores;
+  }, [data, isLoading]);
 
   return {
     supervisores,
-    isLoading: obterSupervisores.isLoading,
-    error: obterSupervisores.error,
+    isLoading: isLoading,
+    error: error,
   };
 }
