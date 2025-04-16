1611d9f18e8e23bba10467f61134addb095b2362
OliverioJunior <oliverio.junior2@gmail.com>
2025-04-15 23:52:37 -0300

    style(TitlePage): update styling for title and link components
    Improve visual hierarchy by making the title bold and removing redundant background color from the link component to maintain consistency with the design system.

--- src/components/TitlePage/index.tsx
+++ src/components/TitlePage/index.tsx
@@ -9,9 +9,9 @@ interface ITitlePage {
 export const TitlePage: FC<ITitlePage> = ({ title }) => {
   return (
     <div className="flex justify-between mt-6">
-      <h1 className="text-2xl">{title}</h1>
+      <h1 className="text-2xl font-bold">{title}</h1>
       <Link
-        className="bg-zinc-300 dark:bg-zinc-700 rounded-full p-1 grid place-items-center hover:bg-zinc-300/75 dark:hover:bg-zinc-300/60 transition duration-300 ease-in-out"
+        className=" rounded-full p-1 grid place-items-center hover:bg-zinc-300/75 dark:hover:bg-zinc-300/60 transition duration-300 ease-in-out"
         href="/dashboard"
       >
         <ArrowLeft />
