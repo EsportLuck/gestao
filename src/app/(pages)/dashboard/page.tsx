5be6cef02a6b3a583629a98b7374ea0426e3560f
OliverioJunior <oliverio.junior2@gmail.com>
2025-04-15 23:46:01 -0300

    refactor(dashboard): simplify page component by using DashboardPage
    The dashboard page component was refactored to use the `DashboardPage` component from the presentation layer, removing redundant logic and improving maintainability. This change centralizes the dashboard logic in a dedicated component, making the code cleaner and easier to manage.

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,38 +1,8 @@
-"use client";
-import React, { FC, useMemo } from "react";
-import { DataTableEstabelecimentos } from "@/components/template";
-import { useSession } from "next-auth/react";
-import { useFetch } from "@/shared/hooks/useFetch";
-import { obterInicioEFimDoCiclo } from "@/app/api/v1/utils/obterInicioEFimDoCiclo";
+import { DashboardPage } from "@/presentation/pages/Dashboard";
+import React, { FC } from "react";
 
 const Dashboard: FC = () => {
-  const { inicioDoCiclo, finalDoCiclo } = obterInicioEFimDoCiclo(new Date());
-  const { data } = useSession();
-  const url = useMemo(() => {
-    if (!data) return null;
-    const queryParams = new URLSearchParams({
-      dataInicial: inicioDoCiclo.toISOString(),
-      dataFinal: finalDoCiclo.toISOString(),
-      localidade: "undefined",
-      secao: "undefined",
-      rota: "undefined",
-      supervisor: "undefined",
-      estabelecimento: "undefined",
-      role: data?.user.role,
-      username: data?.user.username,
-      site: data?.user.site,
-    }).toString();
-
-    return `/api/v1/extract?${queryParams}`;
-  }, [data, inicioDoCiclo, finalDoCiclo]);
-
-  const estabelecimentos = useFetch<{ extrato: any }>(url!, Boolean(url));
-
-  return (
-    <main>
-      <DataTableEstabelecimentos data={estabelecimentos.data?.extrato || []} />
-    </main>
-  );
+  return <DashboardPage />;
 };
 
 export default Dashboard;
