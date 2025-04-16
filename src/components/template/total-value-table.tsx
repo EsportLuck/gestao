4041e37e0f48466e7ac9f89e283c9ae33f6ebc66
OliverioJunior <oliverio.junior2@gmail.com>
2025-04-15 23:52:17 -0300

    refactor(total-value-table): replace flex layout with grid for better responsiveness
    The flex layout was replaced with a grid layout to improve responsiveness and ensure consistent spacing across different screen sizes. This change enhances the user experience by making the table more adaptable to various devices.

--- src/components/template/total-value-table.tsx
+++ src/components/template/total-value-table.tsx
@@ -7,7 +7,7 @@ interface ITotalValueTable {
 
 export const TotalValueTable: FC<ITotalValueTable> = ({ data }) => {
   return (
-    <div className="w-full flex flex-auto justify-between flex-wrap gap-4 mt-8 ">
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 mt-8 ">
       {data?.map((item, index) => (
         <ValueCard key={index} label={item.label} value={item.value} />
       ))}
